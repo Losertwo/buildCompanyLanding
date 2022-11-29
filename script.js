5e471b23f3a664ee0e14a714573f8c79111a3478
Anastasia <105452927+Losertwo@users.noreply.github.com>
2022-11-29 17:47:07 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('.section-questions-accordion__question').forEach((item) =>
-    item.addEventListener('click', () => {
-
-        item.parentNode.classList.toggle('.section-questions-accordion--active')
-    })
-)
